d58838475d6ceb7d85e801fca92b0fa1ca212375
Thelma Boamah <thelma.a.boamah@gmail.com>
2017-02-09 18:26:14 -0800

    connected crud operations in server

--- server.js
+++ server.js
@@ -9,6 +9,7 @@
 //require express in our app
 var express = require('express'),
   bodyParser = require('body-parser');
+          db = require('./models');
 
 // generate a new express app and call it 'app'
 var app = express();
@@ -25,32 +26,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //  DATA
 ///////////////////
 
-var books = [
-  {
-    _id: 15,
-    title: "The Four Hour Workweek",
-    author: "Tim Ferriss",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/four_hour_work_week.jpg",
-    release_date: "April 1, 2007"
-  },
-  {
-    _id: 16,
-    title: "Of Mice and Men",
-    author: "John Steinbeck",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/of_mice_and_men.jpg",
-    release_date: "Unknown 1937"
-  },
-  {
-    _id: 17,
-    title: "Romeo and Juliet",
-    author: "William Shakespeare",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/romeo_and_juliet.jpg",
-    release_date: "Unknown 1597"
-  }
-];
-
-
-var newBookUUID = 18;
 
 
 
@@ -73,64 +48,58 @@ app.get('/', function (req, res) {
 // get all books
 app.get('/api/books', function (req, res) {
   // send all books as JSON response
-  console.log('books index');
-  res.json(books);
+  db.Book.find(function(err, books){
+    if (err) { return console.log("index error: " + err); }
+    res.send(books);
+  });
 });
 
 // get one book
 app.get('/api/books/:id', function (req, res) {
   // find one book by its id
-  console.log('books show', req.params);
-  for(var i=0; i < books.length; i++) {
-    if (books[i]._id === req.params.id) {
-      res.json(books[i]);
-      break; // we found the right book, we can stop searching
-    }
-  }
+  var id = req.params.id;
+  db.Book.find({_id: id}, function(err, book){
+    if (err) { return console.log("index error: " + err); }
+    res.json(book);
+  })
 });
 
 // create new book
 app.post('/api/books', function (req, res) {
   // create new book with form data (`req.body`)
-  console.log('books create', req.body);
-  var newBook = req.body;
-  newBook._id = newBookUUID++;
-  books.push(newBook);
-  res.json(newBook);
+  var title = req.body.title;
+  var author = req.body.author;
+  
+  db.Book.create({title: title, author: author}, function(err, newBook){
+    res.json(newBook);
+  })
 });
 
 // update book
 app.put('/api/books/:id', function(req,res){
 // get book id from url params (`req.params`)
-  console.log('books update', req.params);
-  var bookId = req.params.id;
+  var id = req.params.id;
+  var title = req.body.title;
+  var author = req.body.author;
+  var update = {title: title, author: author};
   // find the index of the book we want to remove
-  var updateBookIndex = books.findIndex(function(element, index) {
-    return (element._id === parseInt(req.params.id)); //params are strings
-  });
-  console.log('updating book with index', deleteBookIndex);
-  var bookToUpdate = books[deleteBookIndex];
-  books.splice(updateBookIndex, 1, req.params);
-  res.json(req.params);
+  console.log(id);
+  db.Book.findOneAndUpdate({_id: id}, update, {new: true}, function(err, updatedBook){
+    if (err) { return console.log("index error: " + err); }
+
+    console.log(updatedBook);
+    res.json(updatedBook);
+  })
 });
 
 // delete book
 app.delete('/api/books/:id', function (req, res) {
   // get book id from url params (`req.params`)
-  console.log('books delete', req.params);
-  var bookId = req.params.id;
-  // find the index of the book we want to remove
-  var deleteBookIndex = books.findIndex(function(element, index) {
-    return (element._id === parseInt(req.params.id)); //params are strings
-  });
-  console.log('deleting book with index', deleteBookIndex);
-  var bookToDelete = books[deleteBookIndex];
-  books.splice(deleteBookIndex, 1);
-  res.json(bookToDelete);
+  var id = req.params.id;
+  db.Book.findOneAndRemove({_id: id}, function(err, deletedBook) {
+    res.json(deletedBook);
+  })  
 });
-
-
-
 
 
 app.listen(process.env.PORT || 3000, function () {
